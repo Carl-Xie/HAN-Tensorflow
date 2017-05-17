759c3ba475ac35f4c04221246d1a02455f859b07
Carl-Xie <359768331@qq.com>
2017-05-17 18:54:22 +0800

    making progress

--- han.py
+++ han.py
@@ -1,96 +1,96 @@
 # encoding=utf-8
 
+from tensorflow.contrib import rnn
+from ops import *
+
 import tensorflow as tf
 
-from tensorflow.contrib import rnn
 
-
-class HierarchicalAttentionNetwork(object):
+class HAN(object):
 
     def __init__(self,
-                 sequence_len,
-                 num_classes,
                  vocab_size,
-                 embedding_size,
-                 num_units):
+                 num_classes,
+                 embedding_size=200,
+                 hidden_size=50,
+                 word_ctx_size=100,
+                 sentence_ctx_size=100):
 
-        self.input_x = tf.placeholder(tf.int32, [None, sequence_len], name='input_x')
-        self.input_y = tf.placeholder(tf.float32, [num_classes], name='input_y')
-        self.dropout_keep_rate = tf.placeholder(tf.float32, name='dropout_keep_rate')
+        self.vocab_size = vocab_size
+        self.embedding_size = embedding_size
+        self.hidden_size = hidden_size
+        self.word_ctx_size = word_ctx_size
+        self.sentence_ctx_size = sentence_ctx_size
+        self.num_classes = num_classes
 
-        # word to vector layer
-        with tf.name_scope('embedding_layer'):
-            self.w_embed = tf.Variable(tf.random_normal((vocab_size, embedding_size)),
+    def predict(self, x, sequence_length=None):
+        embedded_weights = tf.Variable(tf.random_normal((self.vocab_size, self.embedding_size)),
                                        name='embedded_weights')
-            self.embedded_x = tf.nn.embedding_lookup(self.w_embed, self.input_x)
+        embedded_x = tf.nn.embedding_lookup(embedded_weights, x)
+        words_with_attention = self._get_word_attention(embedded_x, sequence_length)
+        sentences_with_attention = self._get_sentence_attention(words_with_attention)
 
-        # word encoder
-        word_cell_fw = rnn.GRUCell(num_units=num_units, input_size=embedding_size)
-        word_cell_bw = rnn.GRUCell(num_units=num_units, input_size=embedding_size)
+        v = tf.reduce_sum(sentences_with_attention, axis=1)
+        W_c = tf.Variable(tf.truncated_normal([self.num_classes, self.hidden_size * 2]), name='class_weights')
+        b_c = tf.Variable(tf.truncated_normal([self.num_classes]))
+        score = tf.matmul(W_c, v, transpose_a=False, transpose_b=True) + b_c
+        return tf.nn.softmax(score, name='predict')
 
+    def _get_word_attention(self, embedded_x, sequence_length):
+        word_cell_fw = rnn.GRUCell(num_units=self.hidden_size, input_size=self.embedding_size)
+        word_cell_bw = rnn.GRUCell(num_units=self.hidden_size, input_size=self.embedding_size)
         # outputs shape: [batch_size, max_time, state_size]
         word_outputs, _ = tf.nn.bidirectional_dynamic_rnn(cell_fw=word_cell_fw,
                                                           cell_bw=word_cell_bw,
-                                                          inputs=self.embedded_x,
+                                                          inputs=embedded_x,
+                                                          sequence_length=sequence_length,
                                                           time_major=False)
         # word = [h_fw, h_bw]
         encoded_words = tf.concat(word_outputs, axis=2, name='encoded_words')
-        encoded_word_dims = num_units * 2
+        encoded_word_dims = self.hidden_size * 2
         # word attention layer
-        self.word_context = tf.Variable(tf.truncated_normal([encoded_word_dims]))
-        self.W_word = tf.Variable(tf.truncated_normal(shape=[encoded_word_dims, encoded_word_dims]),
-                                  name='context_word_weights')
-        self.b_word = tf.Variable(tf.truncated_normal(shape=[encoded_word_dims]), name='context_word_bias')
+        word_context = tf.Variable(tf.truncated_normal([self.word_ctx_size]))
+        W_word = tf.Variable(tf.truncated_normal(shape=[encoded_word_dims, encoded_word_dims]),
+                             name='word_context_weights')
+        b_word = tf.Variable(tf.truncated_normal(shape=[encoded_word_dims]), name='word_context_bias')
 
-        u_w = tf.tanh(self.matrix_batch_vectors_mul(self.W_word, encoded_words) + self.b_word, name='u_w')
-        word_logits = self.batch_vectors_vector_mul(u_w, self.word_context)
+        # U_{it} = tanh(W_w * h_{it} + b_w)
+        U_w = tf.tanh(matrix_batch_vectors_mul(W_word, encoded_words) + b_word,
+                      name='U_w')
+        word_logits = batch_vectors_vector_mul(U_w, word_context)
         word_attention = tf.nn.softmax(logits=word_logits)
         expand_word_attention = tf.expand_dims(word_attention, -1)
-        words_with_attention = encoded_words * expand_word_attention
+        return encoded_words * expand_word_attention
 
-        # sentence encoder
+    def _get_sentence_attention(self, words_with_attention):
         sentences = tf.reduce_sum(words_with_attention, axis=1)
         expand_sentences = tf.expand_dims(sentences, 0)
-        sentence_cell_fw = rnn.GRUCell(num_units=num_units, input_size=encoded_word_dims)
-        sentence_cell_bw = rnn.GRUCell(num_units=num_units, input_size=encoded_word_dims)
+        sentence_cell_fw = rnn.GRUCell(num_units=self.hidden_size, input_size=self.hidden_size * 2)
+        sentence_cell_bw = rnn.GRUCell(num_units=self.hidden_size, input_size=self.hidden_size * 2)
         sentence_outputs, _ = tf.nn.bidirectional_dynamic_rnn(cell_fw=sentence_cell_fw,
                                                               cell_bw=sentence_cell_bw,
                                                               inputs=expand_sentences,
                                                               time_major=False)
         # sentence attention layer
         encoded_sentences = tf.concat(sentence_outputs, axis=2, name='encoded_sentences')
-        encoded_sentence_dims = encoded_word_dims * 2
-        self.sentence_context = tf.Variable(tf.truncated_normal([encoded_sentence_dims]))
-        self.W_sen = tf.Variable(tf.truncated_normal(shape=[encoded_sentence_dims, encoded_sentence_dims],
-                                                     name='context_sentence_weights'))
-        self.b_sen = tf.Variable(tf.truncated_normal(shape=[encoded_sentence_dims]), name='context_sentence_bias')
-        u_s = tf.tanh(self.matrix_batch_vectors_mul(self.W_sen, encoded_sentences) + self.b_sen, name='u_s')
-        sentence_logits = self.batch_vectors_vector_mul(u_s, self.sentence_context)
+        encoded_sentence_dims = self.hidden_size * 2
+        sentence_context = tf.Variable(tf.truncated_normal([encoded_sentence_dims]))
+        W_sen = tf.Variable(tf.truncated_normal(shape=[encoded_sentence_dims, encoded_sentence_dims],
+                                                name='context_sentence_weights'))
+        b_sen = tf.Variable(tf.truncated_normal(shape=[encoded_sentence_dims]), name='context_sentence_bias')
+        U_s = tf.tanh(matrix_batch_vectors_mul(W_sen, encoded_sentences) + b_sen, name='U_s')
+        sentence_logits = batch_vectors_vector_mul(U_s, sentence_context)
         sentence_attention = tf.nn.softmax(logits=sentence_logits)
         expand_sentence_attention = tf.expand_dims(sentence_attention, -1)
-        sentences_with_attention = encoded_sentences * expand_sentence_attention
+        return encoded_sentences * expand_sentence_attention
 
-        document_with_attention = tf.reduce_sum(sentences_with_attention, axis=1)
+    def loss(self, batch_x, batch_seq_len, batch_y):
+        total_loss = tf.constant(0, tf.float32, name='loss')
+        batch_size = 0
+        for x, seq_len, y in zip(batch_x, batch_seq_len, batch_y):
+            p = self.predict(x, seq_len)
+            total_loss += tf.reduce_sum(tf.log(p) * y)
+            batch_size += 1
+        return total_loss / batch_size if batch_size > 0 else total_loss
 
-        self.W_c = tf.Variable(tf.truncated_normal([num_classes, encoded_sentence_dims]), name='class_weights')
-        self.b_c = tf.Variable(tf.truncated_normal([num_classes]))
-        v = tf.matmul(self.W_c, document_with_attention, transpose_a=False, transpose_b=True)
-        p = tf.nn.softmax(v)
-        self.loss = -tf.reduce_sum(tf.log(p) * self.input_y)
 
-    @staticmethod
-    def matrix_batch_vectors_mul(mat, batch_vectors):
-        assert mat.shape[1] == batch_vectors.shape[-1]
-        vectors = tf.reshape(batch_vectors, [-1, batch_vectors.shape[-1].value])
-        res = tf.matmul(mat, vectors, transpose_a=False, transpose_b=True)
-        shape = batch_vectors.shape.as_list()
-        shape[-1] = mat.shape[0].value
-        return tf.reshape(tf.transpose(res), shape)
-
-    @staticmethod
-    def batch_vectors_vector_mul(batch_vectors, vector):
-        assert batch_vectors.shape[-1] == vector.shape[0]
-        expand_vec = tf.expand_dims(vector, -1)
-        mat_vec = tf.reshape(batch_vectors, [-1, batch_vectors.shape[-1].value])
-        res = tf.matmul(mat_vec, expand_vec)
-        return tf.reshape(res, batch_vectors.shape[:-1])
---
+++ train.py
@@ -0,0 +1,52 @@
+# encoding=utf-8
+
+from han import HAN
+
+import tensorflow as tf
+import os
+
+
+# Parameters
+# ==================================================
+
+# Data loading params
+tf.flags.DEFINE_float("dev_sample_percentage", .1, "Percentage of the training data to use for validation")
+tf.flags.DEFINE_string("positive_data_file", "./data/rt-polaritydata/rt-polarity.pos", "Data source for the positive data.")
+tf.flags.DEFINE_string("negative_data_file", "./data/rt-polaritydata/rt-polarity.neg", "Data source for the negative data.")
+
+# Model Hyper parameters
+tf.flags.DEFINE_integer("embedding_dim", 200, "Dimensionality of character embedding (default: 200)")
+
+# Training parameters
+tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
+tf.flags.DEFINE_integer("num_epochs", 50, "Number of training epochs (default: 50)")
+tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
+tf.flags.DEFINE_integer("checkpoint_every", 300, "Save model after this many steps (default: 100)")
+tf.flags.DEFINE_integer("num_checkpoints", 5, "Number of checkpoints to store (default: 5)")
+# Misc Parameters
+tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
+tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
+
+
+def save(saver, sess, checkpoint_dir, step):
+    model_name = "HAN.model"
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+    saver.save(sess, os.path.join(checkpoint_dir, model_name),
+               global_step=step)
+
+
+def load(saver, sess, checkpoint_dir):
+    import re
+    print(" [*] Reading checkpoints...")
+    checkpoint_state = tf.train.get_checkpoint_state(checkpoint_dir)
+    if checkpoint_state and checkpoint_state.model_checkpoint_path:
+        checkpoint_state_name = os.path.basename(checkpoint_state.model_checkpoint_path)
+        saver.restore(sess, os.path.join(checkpoint_dir, checkpoint_state_name))
+        finditer = re.finditer(pattern="(\d+)(?!.*\d)", string=checkpoint_state_name)
+        counter = int(next(finditer).group(0))
+        print(" [*] Success to read {}".format(checkpoint_state_name))
+        return True, counter
+    else:
+        print(" [*] Failed to find a checkpoint")
+        return False, 0
