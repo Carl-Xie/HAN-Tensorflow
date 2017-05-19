b902cde94b6b9564b1723ed817ed2c2c935afd40
Carl-Xie <359768331@qq.com>
2017-05-19 18:58:22 +0800

    data processing

--- train.py
+++ train.py
@@ -5,16 +5,17 @@ from han import HAN
 import tensorflow as tf
 import os
 
-# Parameters
-# ==================================================
 
 # Data loading params
-tf.flags.DEFINE_float("dev_sample_percentage", .1, "Percentage of the training data to use for validation")
-tf.flags.DEFINE_string("positive_data_file", "./data/rt-polaritydata/rt-polarity.pos", "Data source for the positive data.")
-tf.flags.DEFINE_string("negative_data_file", "./data/rt-polaritydata/rt-polarity.neg", "Data source for the negative data.")
+tf.flags.DEFINE_string("positive_data_dir", "data/pos/", "Data source for the positive data.")
+tf.flags.DEFINE_string("negative_data_dir", "data/neg/", "Data source for the negative data.")
+tf.flags.DEFINE_integer("num_classes", 2, "number of classes")
 
 # Model Hyper parameters
 tf.flags.DEFINE_integer("embedding_dim", 200, "Dimensionality of character embedding (default: 200)")
+tf.flags.DEFINE_integer("hidden_size", 50, "Dimensionality of GRU hidden layer (default: 50)")
+tf.flags.DEFINE_integer("word_context_size", 100, "word context vector size at word attention layer")
+tf.flags.DEFINE_integer("sentence_context_size", 100, "sentence context vector at sentence attention layer")
 
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
@@ -22,6 +23,7 @@ tf.flags.DEFINE_integer("num_epochs", 50, "Number of training epochs (default: 5
 tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
 tf.flags.DEFINE_integer("checkpoint_every", 300, "Save model after this many steps (default: 100)")
 tf.flags.DEFINE_integer("num_checkpoints", 5, "Number of checkpoints to store (default: 5)")
+
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
 tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
@@ -38,14 +40,21 @@ def save(saver, sess, checkpoint_dir, step):
 def load(saver, sess, checkpoint_dir):
     import re
     print(" [*] Reading checkpoints...")
-    checkpoint_state = tf.train.get_checkpoint_state(checkpoint_dir)
-    if checkpoint_state and checkpoint_state.model_checkpoint_path:
-        checkpoint_state_name = os.path.basename(checkpoint_state.model_checkpoint_path)
-        saver.restore(sess, os.path.join(checkpoint_dir, checkpoint_state_name))
-        finditer = re.finditer(pattern="(\d+)(?!.*\d)", string=checkpoint_state_name)
+    ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+    if ckpt and ckpt.model_checkpoint_path:
+        ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
+        saver.restore(sess, os.path.join(checkpoint_dir, ckpt_name))
+        finditer = re.finditer(pattern="(\d+)(?!.*\d)", string=ckpt_name)
         counter = int(next(finditer).group(0))
-        print(" [*] Success to read {}".format(checkpoint_state_name))
+        print(" [*] Success to read {}".format(ckpt_name))
         return True, counter
     else:
         print(" [*] Failed to find a checkpoint")
         return False, 0
+
+
+def main(_):
+    pass
+
+if __name__ == '__main__':
+    tf.app.run()
